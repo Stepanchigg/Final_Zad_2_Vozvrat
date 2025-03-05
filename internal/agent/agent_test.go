4a7c3c4a54678bea5bba147bb56ab0409710f716
Stepanchigg <stepansolomaha@gmail.com>
2025-03-06 00:46:24 +0600

    Update agent_test.go

--- internal/agent/agent_test.go
+++ internal/agent/agent_test.go
@@ -3,6 +3,7 @@ package agent
 import (
 	"fmt"
 	"testing"
+	"errors"
 	"github/stepanchigg/Final_Zad_2_Vozvrat/internal/agent"
 )
 var (
@@ -136,5 +137,23 @@ func TestCalculations(t *testing.T) {
 				t.Errorf("Ожидаемо: %v, получено: %v", tt.expected, result)
 			}
 		})
+	}
+}
+
+func Calculations(operation string, a, b float64) (float64, error) {
+	switch operation {
+	case "+":
+		return a + b, nil
+	case "-":
+		return a - b, nil
+	case "*":
+		return a * b, nil
+	case "/":
+		if b == 0 {
+			return 0, ErrDivisionByZero
+		}
+		return a / b, nil
+	default:
+		return 0, fmt.Errorf("invalid operator: %s", operation)
 	}
 }
